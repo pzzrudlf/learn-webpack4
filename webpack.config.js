c0e837349fc3353a38360cf3ba76fd6ef88d8bde
pzzrudlf <pzzrudlf@localhost.localdomain>
2018-05-25 15:01:06 +0800

    success

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// const webpack = require('webpack');
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
